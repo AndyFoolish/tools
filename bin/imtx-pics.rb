fce361ef4f5515be34434cd97606da374850cf44
Tualatrix Chou <tualatrix@gmail.com>
2012-05-01 23:19:11 +0800

    imtx-pics.rb: index from 1, and make sure two bit of num

--- bin/imtx-pics.rb
+++ bin/imtx-pics.rb
@@ -4,6 +4,10 @@ print "Please input the prefix: "
 prefix = gets.strip
 
 Dir.glob('*.JPG').sort.each_with_index do |path, i|
+    i = i + 1
+    if i < 10
+        i = "0#{i}"
+    end
     puts "Converting #{path} to #{prefix}-#{i}.jpg..."
     system("convert -resize 550x413 #{path} #{prefix}-#{i}.jpg")
 end
