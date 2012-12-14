1f0e56938e09ae66b19756b5a98d578201cbb3d9
Tualatrix Chou <tualatrix@gmail.com>
2012-12-14 20:24:16 +0800

    Fix convert_mac_iconset.py, it should be 128x128

--- bin/convert_mac_iconset.py
+++ bin/convert_mac_iconset.py
@@ -2,7 +2,7 @@
 
 import os
 
-size_list = ['16x16', '32x32', '128x123', '256x256', '512x512']
+size_list = ['16x16', '32x32', '128x128', '256x256', '512x512']
 
 for index, size in enumerate(size_list):
     os.system('convert -resize %s icon_512x512@2x.png icon_%s.png' % (size, size))
