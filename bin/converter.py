531170368f5c3ee10465ddda70a737038cb62cb8
Tualatrix Chou <tualatrix@gmail.com>
2014-01-08 22:25:14 +0800

    Add download_picasa_photos_and_upload_to_flickr.sh

--- bin/converter.py
+++ bin/converter.py
@@ -9,9 +9,7 @@ to_converted.sort()
 print("Please enter the prefix: %s")
 prefix = raw_input()
 
-if not os.path.exists(prefix):
-    os.makedirs(prefix)
-
 for i, name in enumerate(to_converted):
     print('convert -resize 550x413 %s %s/%s-%d.jpg' % (name, prefix, prefix, i))
     os.system('convert -resize 550x413 %s %s/%s-%d.jpg' % (name, prefix, prefix, i))
+#    os.system('convert -resize 1024x768 %s/%s-%d.jpg' % (name, prefix, prefix, i))
--- bin/imtx-pics.rb
+++ bin/imtx-pics.rb
@@ -3,11 +3,11 @@
 print "Please input the prefix: "
 prefix = gets.strip
 
-Dir.glob('*.JPG').sort.each_with_index do |path, i|
+sir.glob('*.jpg').sort.each_with_index do |path, i|
     i = i + 1
     if i < 10
         i = "0#{i}"
     end
     puts "Converting #{path} to #{prefix}-#{i}.jpg..."
-    system("convert -resize 550x413 #{path} #{prefix}-#{i}.jpg")
+    system("convert -quality 100 -resize 550x413 #{path} #{prefix}-#{i}.jpg")
 end
